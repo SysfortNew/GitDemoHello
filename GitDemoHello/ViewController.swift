ee2cfa17152745b2c4d1715fed30821131caa9a8
Mac <mac@Macs-Mac-mini-2.local>
2020-03-03 12:53:10 +0530

    Initial Commit

---
+++ GitDemoHello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemoHello
+//
+//  Created by Mac on 03/03/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
